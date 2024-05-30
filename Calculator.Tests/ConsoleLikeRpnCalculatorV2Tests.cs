60d3b552e63c5db17f1a2f86419fee8145baba08
Bejnmain <Bejnmain@5800X>
2024-05-30 23:13:51 +0200

    ConsoleLikeRpnCalculatorV2Tests

---
+++ Calculator.Tests/ConsoleLikeRpnCalculatorV2Tests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using Calculator.RpnCalculatorV2;
+using Calculator.UI;
+using Moq;
+using Xunit;
+
+namespace Calculator.Tests;
+
+public class ConsoleLikeRpnCalculatorV2Tests
+{
+	private readonly Mock<IBasicIO> _mockIo;
+	private readonly ConsoleLikeRpnCalculatorV2 _consoleCalculator;
+
+	public ConsoleLikeRpnCalculatorV2Tests()
+	{
+		_mockIo = new Mock<IBasicIO>();
+		_consoleCalculator = new ConsoleLikeRpnCalculatorV2(_mockIo.Object);
+	}
+
+	[Fact]
+	public void Run_WithValidSequence_PerformsCorrectCalculation()
+	{
+		// Arrange
+		var inputSequence = new Queue<string>(["3", "5", "+", "8", "/", "3", "6", "+", "*", "p", "q"]);
+		_mockIo.Setup(io => io.GetInput()).Returns(inputSequence.Dequeue);
+
+		// Act
+		_consoleCalculator.Run();
+
+		// Assert
+		_mockIo.Verify(io => io.PushOutput(It.Is<string>(s => s.Contains("Result: [ 9 ]"))), Times.Once);
+	}
+
+	[Fact]
+	public void Run_ClearCommand_ClearsCalculator()
+	{
+		// Arrange
+		var inputSequence = new Queue<string>(["3", "5", "+", "c", "q"]);
+		_mockIo.Setup(io => io.GetInput()).Returns(inputSequence.Dequeue);
+
+		// Act
+		_consoleCalculator.Run();
+
+		// Assert
+		_mockIo.Verify(io => io.PushOutput(It.Is<string>(s => s.Contains("[  ]"))), Times.AtLeastOnce);
+	}
+
+	[Fact]
+	public void Run_RemoveLastItemCommand_RemovesLastItem()
+	{
+		// Arrange
+		var inputSequence = new Queue<string>(["3", "5", "+", "r", "q"]);
+		_mockIo.Setup(io => io.GetInput()).Returns(inputSequence.Dequeue);
+
+		// Act
+		_consoleCalculator.Run();
+
+		// Assert
+		_mockIo.Verify(io => io.PushOutput(It.Is<string>(s => s.Contains("Removed: +"))), Times.Once);
+		_mockIo.Verify(io => io.PushOutput("Removed: +"), Times.Once);
+	}
+
+	[Fact]
+	public void Run_InvalidInput_ShowsUnknownInput()
+	{
+		// Arrange
+		var inputSequence = new Queue<string>(["3", "x", "q"]);
+		_mockIo.Setup(io => io.GetInput()).Returns(inputSequence.Dequeue);
+
+		// Act
+		_consoleCalculator.Run();
+
+		// Assert
+		_mockIo.Verify(io => io.PushOutput(It.Is<string>(s => s.Contains("Unknown input: x"))), Times.Once);
+	}
+
+	[Fact]
+	public void Run_ExitCommand_ExitsProgram()
+	{
+		// Arrange
+		var inputSequence = new Queue<string>(["q"]);
+		_mockIo.Setup(io => io.GetInput()).Returns(inputSequence.Dequeue);
+
+		// Act
+		_consoleCalculator.Run();
+
+		// Assert
+		_mockIo.Verify(io => io.GetInput(), Times.Once);
+	}
+}
